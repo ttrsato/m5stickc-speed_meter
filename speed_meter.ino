cfd6ab229c3a1b0bdc37bac5bc85ce5bcd08bb30
ttrsato <tatsurosatoh@gmail.com>
2019-12-26 11:57:48 +0900

    Add speed 0 detection

--- speed_meter.ino
+++ speed_meter.ino
@@ -5,6 +5,11 @@ int64_t circumference_hour = circumference * 60 * 60;
 int64_t last_time;
 volatile int speed;
 
+hw_timer_t *tim0 = NULL;
+enum {RUN, STOP};
+volatile int state = STOP;
+
+// Display speed
 void dispSpeed(int s)
 {
   if (speed > 999) return;  // Ignore over ranged speed
@@ -16,6 +21,16 @@ void dispSpeed(int s)
   M5.Lcd.printf("%d", d);
   M5.Lcd.setCursor(105, 0, 2);
   M5.Lcd.print("km/h");
+}
+
+// Monitor stop state
+// If the status is stopped, clear the speed
+void IRAM_ATTR checkStopState()
+{
+  if (state == STOP) {
+    speed = 0;
+  }
+  state = STOP;
 }
 
 void setup() {
@@ -32,11 +47,18 @@ void setup() {
   // Init LCD display
   M5.Lcd.setRotation(1);
   dispSpeed(0);
-}
+
+  // Timer setting
+  // Monitor stop state
+  tim0 = timerBegin(0, 80, true); // two second
+  timerAttachInterrupt(tim0, &checkStopState, true);
+  timerAlarmWrite(tim0, (1 * 1000000), true); // 1sec
+  timerAlarmEnable(tim0);
+ }
 
 void magnetPassed_Handler()
 {
-  // Stop GPIO interrupt to avoid re-entrant
+  // Stop GPIO interrupt tentatively to avoid re-entrant
   // I'm not sure this code works well or not
   // There may be a better way to do the same thing
   detachInterrupt(GPIO_NUM_26);
@@ -48,6 +70,7 @@ void magnetPassed_Handler()
     // Update speed x 10
     speed = (double)(circumference_hour * 10) / (double)diff_time;
     last_time = cur_time;
+    state = RUN;
     Serial.println(diff_time); // debug
   }
   
